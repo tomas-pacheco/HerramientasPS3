29def98ed8e373eb2c38322448b072e3f1bab17a
1tomaspacheco <69006854+tomas-pacheco@users.noreply.github.com>
2022-07-17 01:38:50 -0300

    Datosclima

---
+++ programas/inicial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 16 20:25:40 2022
+
+@author: Tomas
+"""
+
+from wwo_hist import retrieve_hist_data
+
+#### Set working directory to store output csv file(s)
+import os
+os.chdir("\\Users\\Tomas\\Desktop\\abi\\herramientasclase3")
+
+#### Example code
+frequency = 24 
+start_date = '11-DEC-2018'
+end_date = '11-JAN-2019'
+api_key = '53560d256ace4589bd2235034221607'
+location_list = ['20637','20653','20688','20735','20871','21040',
+                 '21042','21158','21201','21220','21412','21502',
+                 '21601','21638','21639','21643','21651','21703',
+                 '21742','21801','21811','21853','21902']
+
+hist_weather_data = retrieve_hist_data(api_key,
+                                location_list,
+                                start_date,
+                                end_date,
+                                frequency,
+                                location_label = False,
+                                export_csv = True,
+                                store_df = True)
+
