a57f5dcf989d5084df3497f4627f3504aaf79c2a
1tomaspacheco <69006854+tomas-pacheco@users.noreply.github.com>
2022-07-16 23:59:31 -0300

    cambios

---
+++ descarga.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 16 21:41:45 2022
+
+@author: Tomas
+"""
+
+import pandas as pd
+from sodapy import Socrata
+
+
+client = Socrata("odn.data.socrata.com", "VTGB0DNnS1HP1pKSPDRrwkU0A", 
+                 username = "seminarioseconomia@udesa.edu.ar", 
+                 password= "EconUdesa2020+")
+
+
+results = client.get("tt5s-y5fc", limit=2000)
+
+
+results_df = pd.DataFrame.from_records(results)
+results_df.to_csv('crime.csv', header=True, index=False)
+
+
+
+
